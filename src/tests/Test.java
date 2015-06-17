2982c2b4ad1dcc6e5ad6bd86fdd36e8fb0d24e45
Jonathan David Page <jonathan@sleepingcyb.org>
2015-06-17 01:40:06 -0400

    Fix JNI issues
    - fill out JNI FFI types
    - remove fat pointers from Java_ exports
    - experiment with calling into JNIEnv stubs table
    - add type-safe wrappers to JNI FFI types

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -13,6 +13,12 @@ public class Test {
         jniObject.hasArgument(5);
         int x = jniObject.sum(2,3);
         System.out.println(x);
+
+        String s = jniObject.makeString();
+        System.out.println(s);
+
+        String sb = jniObject.concat("hello", "world");
+        System.out.println(sb);
     }
 
 }
